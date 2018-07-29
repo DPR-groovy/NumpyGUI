ea19efb8106b325ae138f237399c846af9de0a23
Kane O'Farrell <41264751+kaneofarrell@users.noreply.github.com>
2018-07-29 08:03:52 +0800

    Add files via upload

--- example.py
+++ example.py
@@ -1,6 +1,69 @@
 import NumpyGUI3
 from NumpyGUI3 import *
 import sys, os
+from collections import deque
+
+class MainToolbar(Qt.QWidget):
+    def __init__(self, parent=None):
+        super(MainToolbar, self).__init__(parent)
+        self.setupUI()
+        self.mountwindow = deque(maxlen=3)
+
+    def setupUI(self):
+        self.setWindowTitle("Main Toolbar")
+        self.setGeometry(0, 0, 300, 100)
+        self.btnTableView1 = Qt.QPushButton("TableView1")
+        self.btnTableView2 = Qt.QPushButton("TableView2")
+        self.btnTableView3 = Qt.QPushButton("TableView3")
+        self.btnTableView1.clicked.connect(self.popupTV1)
+        self.btnTableView2.clicked.connect(self.popupTV2)
+        self.btnTableView3.clicked.connect(self.popupTV3)
+
+        layout = Qt.QVBoxLayout()
+        layout.addWidget(self.btnTableView1)
+        layout.addWidget(self.btnTableView2)
+        layout.addWidget(self.btnTableView3)
+        self.setLayout(layout)
+
+    def popupTV1(self):
+        nptm = NumpyTableModel(np.random.randn(100000, 100))
+        nptv = NumpyTableView(nptm)
+        tablewindow = TableWindow(nptv)
+        self.openWindow(tablewindow)
+
+    def popupTV2(self):
+        nptm = NumpyTableModel(np.load("./data/AMZN.npz")['data'],
+                                                     dtypes={'Open': "{:,.2f}", 'High': "{:,.2f}", 'Low': "{:,.2f}",
+                                                             "Close": "{:,.2f}", "Volume": "{:,.0f}"},
+                                                     alignment={'Date': 'center', 'Code': 'center', 'Open': 'center',
+                                                                'High': 'center', 'Low': 'center', 'Close': 'center'},
+                                                     colormap={'RdBu': [['Open', 'High', 'Low', 'Close'], 'Volume']},
+                                                     alpha=255)
+        nptv = NumpyTableView(nptm)
+        tablewindow = TableWindow(nptv)
+        self.openWindow(tablewindow)
+
+    def popupTV3(self):
+        nptm = NumpyTableModel(np.random.randn(100000, 100))
+        nptv = NumpyTableView(nptm)
+        tablewindow = TableWindow(nptv)
+        self.openWindow(tablewindow)
+
+    def run(self):
+        self.show()
+
+    def openWindow(self, tablewindow):
+        self.mountwindow.append(tablewindow)
+        self.mountwindow[-1].run()
+
+    def closeEvent(self, event):
+        try:
+            for i in range(len(self.mountwindow)):
+                self.mountwindow[i].close()
+            event.accept()
+        except:
+            event.ignore()
+
 
 class TableWindow(Qt.QWidget):
     def __init__(self, tableview, parent=None):
@@ -56,14 +119,17 @@ class TableWindow(Qt.QWidget):
 
 if __name__ == "__main__":
     app = Qt.QApplication(sys.argv)
-    nptm = NumpyTableModel(np.load("./data/AMZN.npz")['data'],
-                                             dtypes={'Open': "{:,.2f}", 'High': "{:,.2f}", 'Low': "{:,.2f}",
-                                                     "Close": "{:,.2f}", "Volume": "{:,.0f}"},
-                                             alignment={'Date': 'center', 'Code': 'center', 'Open': 'center',
-                                                        'High': 'center', 'Low': 'center', 'Close': 'center'},
-                                             colormap={'RdBu': [['Open', 'High', 'Low', 'Close'], 'Volume']},
-                                             alpha=255)
-    nptv = NumpyTableView(nptm)
-    tablewindow = TableWindow(nptv)
-    tablewindow.run()
+#    nptm = NumpyTableModel(np.load("./data/AMZN.npz")['data'],
+#                                             dtypes={'Open': "{:,.2f}", 'High': "{:,.2f}", 'Low': "{:,.2f}",
+#                                                     "Close": "{:,.2f}", "Volume": "{:,.0f}"},
+#                                             alignment={'Date': 'center', 'Code': 'center', 'Open': 'center',
+#                                                        'High': 'center', 'Low': 'center', 'Close': 'center'},
+#                                             colormap={'RdBu': [['Open', 'High', 'Low', 'Close'], 'Volume']},
+#                                             alpha=255)
+#    nptm = NumpyTableModel(np.random.randn(100000, 100))
+#    nptv = NumpyTableView(nptm)
+#    tablewindow = TableWindow(nptv)
+#    tablewindow.run()
+    maintoolbar = MainToolbar()
+    maintoolbar.run()
     app.exec_()
